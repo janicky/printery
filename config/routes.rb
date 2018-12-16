930069f82325cb64e551a23ed06e84561a6556e5
Patryk Janicki <kontakt.janicki@gmail.com>
2018-12-16 18:57:59 +0100

    Create user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  scope module: :api, defaults: { format: :json }, path: "api" do
-    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-      devise_for :users, controllers: {
-        registrations: "api/v1/users/registrations",
-      }, skip: [:sessions, :password]
-    end
-  end
+  # scope module: :api, defaults: { format: :json }, path: "api" do
+  #   scope module: :v1 do
+  #     devise_for :users, controllers: {
+  #       registrations: "api/v1/users/registrations",
+  #     }, skip: [:sessions, :password]
+  #   end
+  # end
 end
