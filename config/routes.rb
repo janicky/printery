61056e538eb4f058585e18543ac616ccd8803b8f
Patryk Janicki <kontakt.janicki@gmail.com>
2018-12-16 19:26:58 +0100

    Add devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
   namespace :api do
     use_doorkeeper
   end
-  # scope module: :api, defaults: { format: :json }, path: "api" do
-  #   scope module: :v1 do
-  #     devise_for :users, controllers: {
-  #       registrations: "api/v1/users/registrations",
-  #     }, skip: [:sessions, :password]
-  #   end
-  # end
+  scope module: :api, defaults: { format: :json }, path: "api" do
+    scope module: :v1 do
+      devise_for :users, controllers: {
+        registrations: "api/v1/users/registrations",
+      }, skip: [:sessions, :password]
+    end
+  end
 end
