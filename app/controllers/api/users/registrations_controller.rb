c9286f975873f916451d66683125ac939a02c2b6
Patryk Janicki <kontakt.janicki@gmail.com>
2018-12-16 19:48:51 +0100

    Add create method to registrations controller

--- app/controllers/api/users/registrations_controller.rb
+++ app/controllers/api/users/registrations_controller.rb
@@ -10,9 +10,18 @@ class Api::Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    build_resource(sign_up_params)
+    resource.save
+    if resource.persisted?
+      expire_data_after_sign_in! unless resource.active_for_authentication?
+      render json: resource
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
+    end
+  end
 
   # GET /resource/edit
   # def edit
