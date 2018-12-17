26002deae3cc8dbc6406ba76ce1edf99a5eda850
Patryk Janicki <kontakt.janicki@gmail.com>
2018-12-17 13:49:11 +0100

    Add company model validation

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,7 @@
 class Company < ApplicationRecord
   has_many :orders, dependent: :destroy
+
+  validates :name, presence: true, length: { minimum: 2, maximum: 50 }
+  validates :tax_number, presence: true, numericality: { only_integer: true }
+  validates :address, presence: true
 end
