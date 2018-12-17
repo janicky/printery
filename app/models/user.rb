d5c860b96aeed53cbc73ee0fb619a628444e28a0
Patryk Janicki <kontakt.janicki@gmail.com>
2018-12-17 22:53:31 +0100

    Add user model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :release_receipts, dependent: :nullify
+
+  validates :first_name, presence: true, length: { minimum: 3, maximum: 30 }
+  validates :last_name, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :password, presence: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
