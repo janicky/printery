90e3f16a0ab8040189869b6319c769fea3d94506
Patryk Janicki <kontakt.janicki@gmail.com>
2018-12-17 22:44:12 +0100

    Add operation model validation

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -1,4 +1,9 @@
 class Operation < ApplicationRecord
   belongs_to :order
   belongs_to :machine
+
+  validates :order, presence: true
+  validates :machine, presence: true
+  validates :status, presence: true, inclusion: { in: [*0...3] }
+  validates :priority, presence: true, numericality: { only_integer: true }
 end
