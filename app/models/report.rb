0c8691ab32fbe252328519b317811190f2621a6f
Patryk Janicki <kontakt.janicki@gmail.com>
2018-12-17 22:51:00 +0100

    Add report model validation

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,2 +1,8 @@
 class Report < ApplicationRecord
+  REGEX = /\A\d+(?:\.\d{0,2})?\z/.freeze
+
+  validates :from, presence: true
+  validates :to, presence: true
+  validates :income, presence: true, format: { with: REGEX }
+  validates :orders, presence: true, numericality: { only_integer: true }
 end
