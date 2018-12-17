ff2adcb4d40b323292af3c4ec5686cc4d802ddda
Patryk Janicki <kontakt.janicki@gmail.com>
2018-12-17 22:26:09 +0100

    Add order model validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,6 +6,17 @@ class Order < ApplicationRecord
   belongs_to :paper
   belongs_to :color_palette
 
+  REGEX = /\A\d+(?:\.\d{0,2})?\z/.freeze
+
+  validates :company, presence: true
+  validates :price, presence: true, format: { with: REGEX }, numericality: { greater_than: 0 }
+  validates :count, presence: true, numericality: { only_integer: true }
+  validates :deadline, presence: true
+  validates :description, presence: true, length: { minimum: 5, maximum: 255 }
+  validates :paper_size, presence: true
+  validates :paper, presence: true
+  validates :color_palette, presence: true
+
   def status
     status = 0
     return status if operations.count == 0
--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,6 +1,9 @@
 class Paper < ApplicationRecord
   has_many :orders, dependent: :nullify
 
+  REGEX = /\A\d+(?:\.\d{0,2})?\z/.freeze
+
   validates :name, presence: true, length: { minimum: 2, maximum: 40 }
   validates :grammage, presence: true, numericality: { only_integer: true }
+  validates :price, presence: true, format: { with: REGEX }, numericality: { greater_than: 0 }
 end
