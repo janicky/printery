bbe3ba4e123dcca7614aa7ddbbc07887731e6233
Patryk Janicki <kontakt.janicki@gmail.com>
2018-12-17 12:51:06 +0100

    Add virtual status attribute to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,14 @@ class Order < ApplicationRecord
   belongs_to :paper_size
   belongs_to :paper
   belongs_to :color_palette
+
+  def status
+    status = 0
+    return status if operations.count == 0
+
+    status = 1 if operations.where(status: 1).count > 0
+    status = 2 if operations.where(status: 2).count == operations.count
+    status = 3 if operations.where(status: 3).count == operations.count
+    status
+  end
 end
