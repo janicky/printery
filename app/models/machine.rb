9561bd82a00f8d61f818c63c78079163e641c120
Patryk Janicki <kontakt.janicki@gmail.com>
2018-12-17 22:35:38 +0100

    Add machine model validation

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -1,4 +1,8 @@
 class Machine < ApplicationRecord
   self.inheritance_column = "kind"
   has_many :operations, dependent: :nullify
+
+  validates :name, presence: true, length: { minimum: 2, maximum: 10 }
+  validates :type, presence: true, inclusion: { in: %w[printer cutter gluing refinment] }
+  validates :since_cleaning, numericality: { only_integer: true }
 end
