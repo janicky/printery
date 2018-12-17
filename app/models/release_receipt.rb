9e1fc55507d1d23e906acdb3720e30c0a03edc71
Patryk Janicki <kontakt.janicki@gmail.com>
2018-12-17 22:45:52 +0100

    Add release receipt model validation

--- app/models/release_receipt.rb
+++ app/models/release_receipt.rb
@@ -1,4 +1,11 @@
 class ReleaseReceipt < ApplicationRecord
   belongs_to :order
   belongs_to :user
+
+  REGEX = /\A\d+(?:\.\d{0,2})?\z/.freeze
+
+  validates :order, presence: true
+  validates :release_date, presence: true
+  validates :user, presence: true
+  validates :price, presence: true, format: { with: REGEX }, numericality: { greater_than: 0 }
 end
