12d65287d7390565c731036244c279f1fddfc469
Patryk Janicki <kontakt.janicki@gmail.com>
2018-12-17 22:06:47 +0100

    Add paper model validation

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,3 +1,6 @@
 class Paper < ApplicationRecord
   has_many :orders, dependent: :nullify
+
+  validates :name, presence: true, length: { minimum: 2, maximum: 40 }
+  validates :grammage, presence: true, numericality: { only_integer: true }
 end
